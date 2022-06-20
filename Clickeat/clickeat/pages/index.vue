65e98835a14b3797c7b888de95f3ff187bf2f635
Telchor <telchor.cuthalion@gmail.com>
2022-06-20 15:57:04 +0200

    Home alignment error fixed

--- Clickeat/clickeat/pages/index.vue
+++ Clickeat/clickeat/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Background">
     <div>
-      <h1 class="display-1" justify="center" align="center">Commandez!</h1>
+      <h1 class="display-1" justify="center">Commandez!</h1>
       <h2 justify="center" align="center">Produits disponibles</h2>
       <v-container class="grey lighten-5 mb-6">
         <v-row :align="align" no-gutters style="height: 60px">
