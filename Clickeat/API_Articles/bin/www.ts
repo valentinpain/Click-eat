c8136a8f7638653daa413defadae909d551001d8
valentinpain <valentin.pain@viacesi.fr>
2022-06-22 14:36:16 +0200

    api ports fix

--- Clickeat/API_Articles/bin/www.ts
+++ Clickeat/API_Articles/bin/www.ts
@@ -12,7 +12,7 @@ import http from 'http';
  * Get port from environment and store in Express.
  */
 
-let port = normalizePort(process.env.PORT || '5000');
+let port = normalizePort(process.env.PORT || '8000');
 app.set('port', port);
 
 /**
