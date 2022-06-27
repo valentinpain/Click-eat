f2ec66fe39e692f4ffb2e4596b1dfd9af546618f
valentinpain <valentin.pain@viacesi.fr>
2022-06-27 14:55:55 +0200

    fix delivery

--- Clickeat/clickeat/pages/menu-creation/_brand.vue
+++ Clickeat/clickeat/pages/menu-creation/_brand.vue
@@ -32,9 +32,11 @@
                 </v-row>
 
                   <v-row>
-                      <v-col></v-col>
                       <v-col>
                         <v-btn @click="createMenu" class="green white--text text-h6">Envoyer</v-btn>
+                      </v-col>
+                      <v-col>
+                        <v-btn class="orange white--text text-h6" :to="'/restaurants/' + $route.params.brand">Retour</v-btn>
                       </v-col>
                     </v-row>
                   <p class="mt-5 ml-5 pink--text text-h6">{{ formResult }}</p>
